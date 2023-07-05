3fc3fc9b8ecbcd986bafd4d794e9bdc10d558a57
Dmitriy <saenko.3344@gmail.com>
2023-07-05 21:55:57 +0300

    initial condition

---
+++ src/main/java/ru/relex/relexsummerpractice/RelexSummerPracticeHomeworkApplication.java
@@ -0,0 +1,13 @@
+package ru.relex.relexsummerpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RelexSummerPracticeHomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RelexSummerPracticeHomeworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/relex/relexsummerpractice/RelexSummerPracticeHomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.relex.relexsummerpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RelexSummerPracticeHomeworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
